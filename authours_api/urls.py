70958050a30936f5e3229f9ae72f7e0819dcb1de
phuchh <phuchh27@gmail.com>
2025-08-27 16:50:23 +0700

    feat: Implement custom user model and admin interface; update database settings and add API documentation

--- authours_api/urls.py
+++ authours_api/urls.py
@@ -1,6 +1,29 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from django.conf import settings
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+from rest_framework import permissions
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Authors Haven API",
+        default_version="v1",
+        description="API documentation for Authors Haven",
+        contact=openapi.Contact(email="api.phuchh27@gmail.com"),
+        license=openapi.License(name="BSD License"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,)
+)
+
 urlpatterns = [
+    path("redoc/", schema_view.with_ui("redoc", cache_timeout=0)),
     path(settings.ADMIN_URL, admin.site.urls),
 ]
+
+admin.site.site_header = "Authors Haven API Admin"
+
+admin.site.site_title = "Authors Haven API Admin Portal"
+
+admin.site.index_title = "Welcome to Authors Haven API Admin Portal"
